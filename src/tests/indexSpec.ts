82dc9874e515158e4c8f258beb2af93221a01e33
Hussein Samy <hussein.samy02@gmail.com>
2022-08-11 16:23:49 +0200

    Adds tests to countries api

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,5 @@
 import myFunc from '../testing_practice'
-
+import countries from '../countries_api'
 
 describe('Suite for myFunc function', () => {
 
@@ -10,6 +10,29 @@ describe('Suite for myFunc function', () => {
   it('expects(10) to equal 100', () => {
       expect(myFunc(10)).toEqual(10*10)
   })
-});
+})
 
 
+describe ('Suite for countries api specs', () => {
+
+  it("should get basic data on the country canada", async () => {
+    const data = await countries.getCountry('canada');
+    expect(data).toEqual({
+      capital: 'Ottawa',
+      region: 'Americas',
+      numericCode: '124'
+    });
+  });
+  
+  it("should get capitals of NAFTA countries", async () => {
+    const data = await countries.getRegionCapitals('nafta');
+    expect(data).toEqual([
+      'Ottawa', 'Mexico City', 'Washington, D.C.'
+    ]);
+  });
+  
+  it('should get region countries of new york', async() =>{
+      const data = await countries.getRegionCountries('New-York');
+      expect(data).toEqual(['first country', 'second country', 'and so on']);
+  } )
+})
