b931e9981a522913766721b23897e5d6d05f447e
Hussein Samy <hussein.samy02@gmail.com>
2022-08-11 16:13:11 +0200

    Adds suite for myFunc in test files

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,9 +1,15 @@
 import myFunc from '../testing_practice'
 
-it('expect myFunc(5) to equal 25', () => {
-  expect(myFunc(5)).toEqual(25);
+
+describe('Suite for myFunc function', () => {
+
+  it('expect myFunc(5) to equal 25', () => {
+    expect(myFunc(5)).toEqual(25);
+  });
+  
+  it('expects(10) to equal 100', () => {
+      expect(myFunc(10)).toEqual(10*10)
+  })
 });
 
-it('expects(10) to equal 100', () => {
-    expect(myFunc(10)).toEqual(10*10)
-})
+
