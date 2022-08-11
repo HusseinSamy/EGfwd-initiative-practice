7325e6ccd1a97c8ca20699dd4cd9b3289a3d87fa
Hussein Samy <hussein.samy02@gmail.com>
2022-08-11 23:23:42 +0200

    Adds next function to the logging middleware

--- src/middlewares/loggingMiddleware.ts
+++ src/middlewares/loggingMiddleware.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 const logger = (req:Request, res:Response, next:NextFunction) => {
     res.send(`[${req.baseUrl}] has beed visited!`);
     console.log(`[${req.baseUrl}] has beed visited!`);
+    next();
 }
 
 export default logger;
