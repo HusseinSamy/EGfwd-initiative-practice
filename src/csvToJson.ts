10bab2d41a072514584d8ee02ca680954aad8d90
Hussein Samy <hussein.samy02@gmail.com>
2022-08-12 15:03:08 +0200

    Writes csv json data to the output file

--- src/csvToJson.ts
+++ src/csvToJson.ts
@@ -1,27 +1,33 @@
 import express from 'express';
 import csv from 'csvtojson'
+import {promises as fsPromises}  from 'fs'
 
-const csvFilePath='C:/Users/Weshkl/Desktop/EGfwd initiative practice/assets/users.csv'
-
-
-
+const csvFilePath = 'C:/Users/Weshkl/Desktop/EGfwd initiative practice/assets/users.csv'
+const outputFile = './output/data.txt';
 const app = express();
 const port = 3000;
 
 app.get('/convert', async(req, res) => {
-    
-    csv()
-    .fromFile(csvFilePath)
-    .then((jsonObj)=>{
-        console.log(jsonObj);
-    })
-    const jsonArray = await csv().fromFile(csvFilePath);
-    jsonArray.forEach(e => {
-        if(e['phone'] === ''){
-            e['phone'] = 'missing phone';
-        }
-    });
-    res.send(jsonArray);
+    try{
+
+        csv()
+        .fromFile(csvFilePath)
+        .then((jsonObj)=>{
+            console.log(jsonObj);
+        })
+        const jsonArray = await csv().fromFile(csvFilePath);
+        jsonArray.forEach(e => {
+            if(e['phone'] === ''){
+                e['phone'] = 'missing phone';
+            }
+        });
+        
+        const file = await fsPromises.writeFile(outputFile,JSON.stringify(jsonArray));
+        res.send(jsonArray);
+    }
+    catch(error) {
+        console.log(error)
+    }
 })
 
 app.listen(port, () => {
