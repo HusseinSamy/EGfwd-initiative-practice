ef68aa045a5e6ac1d9007d3b75b56233d21e00bb
Hussein Samy <hussein.samy02@gmail.com>
2022-08-19 15:53:23 +0200

    Renames dataBaseConnection to database

---
+++ src/_Section_2/database.ts
@@ -0,0 +1,43 @@
+import {Pool} from 'pg';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+const {
+POSTGRES_DB,
+POSTGRES_USER,
+POSTGRES_PASSWORD,
+POSTGRES_HOST,
+POSTGRES_TEST,
+ENV
+} = process.env;
+
+let database: Pool = new Pool({
+    database: POSTGRES_TEST,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    host: POSTGRES_HOST
+});
+
+if (ENV === 'dev') {
+    
+    database = new Pool({
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+        host: POSTGRES_HOST
+    });
+}
+
+if(ENV === 'test') {
+    database = new Pool({
+        database: POSTGRES_TEST,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+        host: POSTGRES_HOST
+    }); 
+}
+
+
+export default database;
