cea170db313bae4ae0556dd316212c613609fbcd
Hussein Samy <hussein.samy02@gmail.com>
2022-08-19 18:18:41 +0200

    Fixes errors in the sql query

--- src/models/bookModel.ts
+++ src/models/bookModel.ts
@@ -15,10 +15,10 @@ export class BookStore {
     async post(title: string, author: string, total_pages: number, type: string, summary: string): Promise<IBook[]> {
         try{
             const connection = await database.connect();
-            const query = `INSERT INTO books (title, author, total_pages, type, summary) VALUES (${title}, ${author}, ${total_pages}, ${type}, ${summary})`;
-            const result = await connection.query(query);
+            const query = `INSERT INTO books (title, author, total_pages, type, summary) VALUES ($1,$2,$3,$4,$5)`;
+            const result = await connection.query(query, [title,author,total_pages,type,summary]);
             connection.release();
-            return result.rows;
+            return result.rows[0];
         }
         catch(err) {
             throw new Error(`Cannot post to the database: ${err}`);
@@ -38,11 +38,11 @@ export class BookStore {
         }
     }
 
-    async update(id: number, col_name: string, new_value: (string | number)): Promise<IBook[]> {
+    async update(new_value: string , id: string): Promise<IBook[]> {
         try{
             const connection = await database.connect();
-            const query = `UPDATE TABLE books SET ${col_name} = ${new_value} WHERE id = ${id}`;
-            const result = await connection.query(query);
+            const query = `UPDATE books SET author = $1 WHERE id = $2`;
+            const result = await connection.query(query,[new_value,id]);
             connection.release();
             return result.rows;
         }
@@ -51,13 +51,13 @@ export class BookStore {
         }
     }
     
-    async delete(id: number): Promise<IBook[]> {
+    async delete(id: string): Promise<IBook> {
         try{
             const connection = await database.connect();
-            const query = `DELETE FROM books WHERE id = ${id}`;
-            const result = await connection.query(query);
+            const query = `DELETE FROM books WHERE id = ($1)`;
+            const result = await connection.query(query,[id]);
             connection.release();
-            return result.rows;
+            return result.rows[0];
         }
         catch(err) {
             throw new Error(`Cannot delete from the database: ${err}`);
