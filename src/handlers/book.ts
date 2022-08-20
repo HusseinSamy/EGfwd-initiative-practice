08de2a01e683f4bd1e2f6e3bddb05aac0275d1a5
Hussein Samy <hussein.samy02@gmail.com>
2022-08-20 02:05:44 +0200

    Adds book handler functions

---
+++ src/handlers/book.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response } from 'express';
+import {IBook, BookStore} from '../models/book';
+
+const books = new BookStore();
+
+const index = async (_req: Request, res: Response) => {
+    const result = await books.index();
+    res.json(result);
+};
+
+const update = async (req: Request, res: Response) => {
+    const result = await books.update('Mike Tyson',req.params.id);
+    res.json(result);
+};
+
+const del = async (req: Request, res: Response) => {
+    const result = await books.delete(req.params.id);
+    res.json(result);
+};
+
+const post = async (_req: Request, res: Response) => {
+    const result = await books.post('The art of thinking clearly', 'Rolf Dobelli', 328, 'Phsychology', 'The book talks about 99 cognitive biases and thinking fallacies in conceise way.');
+    res.json(result);
+};
+
+
+const bookRouter = (app: Application) => {
+    app.get('/books', index);
+    app.post('/books', post);
+    app.delete('/books/:id', del);
+    app.put('/books/:id', update);
+}
+
+export default bookRouter;
