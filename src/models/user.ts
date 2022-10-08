aece46828a12ebfc10eee450a2f06451a6666c6d
Hussein Samy <hussein.samy02@gmail.com>
2022-10-08 23:40:35 +0200

    Fixes Sequelize

--- src/models/user.ts
+++ src/models/user.ts
@@ -16,12 +16,12 @@ export class UserClass {
             const db = await database.connect();
             const sql = 'INSERT INTO users (username, password) VALUES ($1,$2) RETURNING *';
             const hash = bcrypt.hashSync(u.password + pepper, +saltRounds!);
-            const res = await db.query(sql,[u.username, hash]);
+        const res = await db.query(sql,[u.username, hash]);
             db.release();
             return res.rows[0];
         }
         catch(err){
-            throw new Error(`unable to create your accout: ${err}`);
+            throw new Error(`unable to create your account: ${err}`);
         }
     }
 }
